38df269d8f205bbdea4393f55fe663132f57cd51
Lavanya <lcffk@mail.umkc.edu>
2017-02-01 20:40:41 -0600

    Add files via upload

---
+++ LAB 2/Source/validate.js
@@ -0,0 +1,15 @@
+/**
+ * Created by sandeep on 2/1/2017.
+ */
+<script>
+function validate() {
+    var isValid = true;
+    //perform your validation on form and update isValid variable accordingly.
+
+    if(isValid) {
+        window.location = ''; // your desired location
+    }
+
+    return false; // always return false to prevent form from submission.
+}
+</script>
